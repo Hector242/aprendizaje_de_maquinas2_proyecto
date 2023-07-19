5c4cf3d2bcb65fcd7cccf4b7ed98a29e4c1c9f14
HectorSanchez <hsanchez@netrixllc.com>
2023-07-19 18:17:28 -0300

    add: add comments and more data transformations

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,7 +3,7 @@ feature_engineering.py
 
 COMPLETAR DOCSTRING
 
-DESCRIPTIÓN: In this code feature engineering for the dataset It's been done 
+DESCRIPTIÓN: feature engineering.py it's for preprocess the data for ML usage 
 AUTOR: Hector Sanchez
 FECHA:-
 """
@@ -20,8 +20,10 @@ class FeatureEngineeringPipeline(object):
 
     def read_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING 
-        In this function we read the whole dataset
+        DOCSTRING
+
+        In this function load & build the dataset
+
         :return pandas_df: The desired DataLake table as a DataFrame
         :rtype: pd.DataFrame
         """
@@ -41,25 +43,45 @@ class FeatureEngineeringPipeline(object):
     
     def data_transformation(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING
+        DOCSTRING
         
+        This function preprosses the raw dataframe and return another 
+        dataframe with the data transformed 
+
+        :param df: Dataframe to be transform
+        :type df: Dataframe
+
+        :return df_transformed: Dataframe transformed
+        :rtype: Dataframe 
         """
         
-        # FE: Calculing how old is the shop (current year - Establishment_Year)
+        # FEATURES ENGINEERING: Calculing how old is the shop (current year - Establishment_Year)
         df['Outlet_Establishment_Year'] = 2020 - df['Outlet_Establishment_Year']
 
         # Cleaning: Unifying labels for 'Item_Fat_Content'
         df['Item_Fat_Content'] = df['Item_Fat_Content'].replace({'low fat':  'Low Fat', 
-                                                                     'LF': 'Low Fat', 
-                                                                     'reg': 'Regular'})
+                                                                 'LF': 'Low Fat', 
+                                                                 'reg': 'Regular'})
 
-        # cleaning: Imputing with similar values.
-        item_list = list(df[df['Item_Weight'].isnull()]['Item_Identifier'].unique()) # making a list
+        # cleaning: Imputing missing data for Item_Weight with the mode.
+        item_list = list(df[df['Item_Weight'].isnull()]['Item_Identifier'].unique())
         for item in item_list:
-            mode = (df[df['Item_Identifier'] == item][['Item_Weight']]).mode().iloc[0,0] # taking the mode
-            df.loc[df['Item_Identifier'] == item, 'Item_Weight'] = mode # Imputing with the mode
+            mode = (df[df['Item_Identifier'] == item][['Item_Weight']]).mode().iloc[0,0] 
+            df.loc[df['Item_Identifier'] == item, 'Item_Weight'] = mode 
         
-        
+        # Cleaning: Imputing missing data for Outlet_Size with the value = "small"
+        outlet_list = list(df[df['Outlet_Size'].isnull()]['Outlet_Identifier'].unique())
+        for outlet in outlet_list:
+            df.loc[df['Outlet_Identifier'] == outlet, 'Outlet_Size'] =  'Small'
+
+        # FEATURES ENGINEERING: setting new categorie for 'Item_Fat_Content'
+        product_list = ['Household', 'Health and Hygiene', 'Hard Drinks',
+                        'Hard Drinks', 'Soft Drinks', 'Fruits and Vegetables']
+        for product in product_list:
+            df.loc[df['Item_Type'] == product_list[product], 'Item_Fat_Content'] = 'NA'
+
+
+        df_transformed = None
         return df_transformed
 
     def write_prepared_data(self, transformed_dataframe: pd.DataFrame) -> None:
@@ -78,7 +100,6 @@ class FeatureEngineeringPipeline(object):
         df_transformed = self.data_transformation(df)
         self.write_prepared_data(df_transformed)
 
-  
 if __name__ == "__main__":
     FeatureEngineeringPipeline(input_path = '../data',
                                output_path = '../outputs/').run()
